c18fb1c1cdcaec9cb88a044ca3b0ed4e7689b0d7
Muhammad Hamza <mh408800@gmail.com>
2023-12-05 10:28:17 +0500

    dotenv

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
+require("dotenv").config()
+const PORT = process.env.PORT || 3000;
 
 
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+// server
+app.listen(process.env.PORT, ()=>{
+    console.log(`server running at localhost:/${PORT}`)
+})
