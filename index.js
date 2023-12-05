8f95d422eb0c2f3bbee26334acd97cc38c5499c4
Muhammad Hamza <mh408800@gmail.com>
2023-12-05 10:35:43 +0500

    database connection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config()
 const PORT = process.env.PORT || 3000;
+const dbConnection = require("./database/connection")
 
 
 
